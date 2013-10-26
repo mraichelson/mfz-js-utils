1726fe5ed55e2cc40108fb98825652b90a01595a
Michael Raichelson <i.am@michaelraichelson.com>
2013-10-25 22:53:47 -0500

    added example for simple combat

--- examples/simple_combat.js
+++ examples/simple_combat.js
@@ -5,3 +5,50 @@
 var MFZ;
 MFZ = require('../mfz.utils.js');
 
+/**
+ * Assumption: Two frames with one a standard "soldier" loadout.
+ * Assumption: Each frame is attacking the opposite and a different
+ *   frame has applied a spotting die to them (with the value 
+ *   rolled below).
+ * Ignore wild dice for the moment.
+ */
+var attacker, defender, chart;
+attacker = {
+  red : MFZ.dice.getResult(MFZ.dice.rollNdX(2)),
+  blue : MFZ.dice.getResult(MFZ.dice.rollNdX(1)),
+  yellow : MFZ.dice.getResult(MFZ.dice.rollNdX(1)),
+  green : MFZ.dice.getResult(MFZ.dice.rollNdX(1))
+};
+defender = {
+  red : MFZ.dice.getResult(MFZ.dice.rollNdX(2)),
+  blue : MFZ.dice.getResult(MFZ.dice.rollNdX(1)),
+  yellow : MFZ.dice.getResult(MFZ.dice.rollNdX(1)),
+  green : MFZ.dice.getResult(MFZ.dice.rollNdX(1))
+};
+chart = 'standard'; // which damage chart to use.
+
+console.log('Attacker');
+console.log(attacker);
+console.log('Defender');
+console.log(defender);
+console.log('----');
+
+console.log('Resolving initial attack...');
+if (attacker.red + attacker.yellow > defender.blue) {
+  console.log('Attacker: Attack successful');
+  var damageResult = MFZ.dice.rollDamage(attacker.red + attacker.yellow - defender.blue, chart);
+  console.log('Damage result: ' + damageResult);
+}
+else {
+  console.log('Attacker: Attack failed.');
+}
+
+console.log('Resolving retaliatory attack...');
+if (defender.red + defender.yellow > attacker.blue) {
+  console.log('Defender: Attack successful');
+  var damageResult = MFZ.dice.rollDamage(defender.red + defender.yellow - attacker.blue, chart);
+  console.log('Damage result: ' + damageResult);
+}
+else {
+  console.log('Defender: Attack failed.')
+}
